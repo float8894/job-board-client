4d2e99a128ada384761c36500f8f45d8bfe0e245
Abhishek <abhishekpanchal8894@gmail.com>
2025-11-25 16:38:39 +0530

    create useCreateJob hook

--- src/lib/graphql/hooks.js
+++ src/lib/graphql/hooks.js
@@ -1,5 +1,6 @@
-import { useQuery } from '@apollo/client/react';
+import { useMutation, useQuery } from '@apollo/client/react';
 import { companyByIdQuery, jobByIdQuery, jobsQuery } from './queries';
+import { createJobMutation } from './mutations';
 
 export const useCompany = (id) => {
   const { data, loading, error } = useQuery(companyByIdQuery, {
@@ -21,4 +22,24 @@ export const useJobs = () => {
   });
   console.log('Jobs:', data?.jobs);
   return { jobs: data?.jobs, loading, error: Boolean(error) };
+};
+
+export const useCreateJob = () => {
+  const [mutate, { loading }] = useMutation(createJobMutation);
+  const createJob = async (title, description) => {
+    const {
+      data: { job },
+    } = await mutate({
+      variables: { input: { title, description } },
+      update: (cache, { data }) => {
+        cache.writeQuery({
+          query: jobByIdQuery,
+          variables: { id: data.job.id },
+          data,
+        });
+      },
+    });
+    return job;
+  };
+  return { createJob, loading };
 };
--- src/pages/CreateJobPage.jsx
+++ src/pages/CreateJobPage.jsx
@@ -1,29 +1,16 @@
 import { useState } from 'react';
-import { createJobMutation } from '../lib/graphql/mutations';
 import { useNavigate } from 'react-router';
-import { useMutation } from '@apollo/client/react';
-import { jobByIdQuery } from '../lib/graphql/queries';
+import { useCreateJob } from '../lib/graphql/hooks';
 
 function CreateJobPage() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [mutate, { loading }] = useMutation(createJobMutation);
+  const { createJob, loading } = useCreateJob();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const {
-      data: { job },
-    } = await mutate({
-      variables: { input: { title, description } },
-      update: (cache, { data }) => {
-        cache.writeQuery({
-          query: jobByIdQuery,
-          variables: { id: data.job.id },
-          data,
-        });
-      },
-    });
+    const job = await createJob(title, description);
     console.log('[CreateJobPage] Job created:', job);
     navigate(`/jobs/${job.id}`);
   };
